df4c1a4f1d8db77f8c5854716d5c6de0283d9a78
mike7d7 <mike7d7@proton.me>
2025-07-31 06:27:04 -0600

    Add Nix flake (#6)
    * add flake
    
    * minor fixes to flake
    
    * fix flake
    
    * add result/ to .gitignore

---
+++ flake.nix
@@ -0,0 +1,79 @@
+{
+  description = "Wayland screen locker focused on customizability";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+
+    in
+    {
+      packages = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "cthulock";
+            version = "0.1.0-git";
+            src = self;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+            nativeBuildInputs = with pkgs; [
+              rustPlatform.bindgenHook
+              pkg-config
+            ];
+            buildInputs = with pkgs; [
+              libxkbcommon
+              linux-pam
+              libGL
+              wayland
+              makeWrapper
+              fontconfig
+            ];
+            LD_LIBRARY_PATH = "${pkgs.libGL}/lib";
+            cargoBuildType = "debug";
+            cargoCheckType = "debug";
+
+            dontStrip = true;
+
+            postInstall = ''
+              wrapProgram $out/bin/cthulock \
+                --prefix LD_LIBRARY_PATH : "${pkgs.wayland}/lib" \
+                --prefix LD_LIBRARY_PATH : "${pkgs.libGL}/lib" \
+                --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath [ pkgs.fontconfig ]}"
+            '';
+          };
+        }
+      );
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              pkg-config
+              libxkbcommon
+              wayland
+
+              cargo
+              rustc
+              rust-analyzer
+              rustfmt
+              labwc
+            ];
+          };
+        }
+      );
+    };
+}
