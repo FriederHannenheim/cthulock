4f0c0b30d5afd90986afb7f722f4fbf59053794b
Frieder Hannenheim <git@fhannenheim.net>
2025-07-31 16:29:16 +0200

    Fix flake.nix

--- flake.nix
+++ flake.nix
@@ -1,79 +1,78 @@
 {
   description = "Wayland screen locker focused on customizability";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
     let
       supportedSystems = [
         "x86_64-linux"
         "aarch64-linux"
       ];
-      forEachSupportedSystem =
-        f:
-        nixpkgs.lib.genAttrs supportedSystems (
-          system:
-          f {
-            pkgs = import nixpkgs { inherit system; };
-          }
-        );
-
     in
-    {
-      packages = forEachSupportedSystem (
-        { pkgs }:
-        {
-          default = pkgs.rustPlatform.buildRustPackage {
-            pname = "cthulock";
-            version = "0.1.0-git";
-            src = self;
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-            };
-            nativeBuildInputs = with pkgs; [
-              rustPlatform.bindgenHook
-              pkg-config
-            ];
-            buildInputs = with pkgs; [
-              libxkbcommon
-              linux-pam
-              libGL
-              wayland
-              makeWrapper
-              fontconfig
-            ];
-            LD_LIBRARY_PATH = "${pkgs.libGL}/lib";
-            cargoBuildType = "debug";
-            cargoCheckType = "debug";
+    flake-utils.lib.eachSystem supportedSystems (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        nativeBuildInputs = with pkgs; [
+          rustPlatform.bindgenHook
+          pkg-config
+        ];
+        buildInputs = with pkgs; [
+          libclang
+          libxkbcommon
+          linux-pam
+          libGL
+          wayland
+          makeWrapper
+          fontconfig
+        ];
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "cthulock";
+          version = "0.1.0-git";
+          src = self;
 
-            dontStrip = true;
+          inherit buildInputs nativeBuildInputs;
 
-            postInstall = ''
-              wrapProgram $out/bin/cthulock \
-                --prefix LD_LIBRARY_PATH : "${pkgs.wayland}/lib" \
-                --prefix LD_LIBRARY_PATH : "${pkgs.libGL}/lib" \
-                --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath [ pkgs.fontconfig ]}"
-            '';
+          cargoLock = {
+            lockFile = ./Cargo.lock;
           };
-        }
-      );
-      devShells = forEachSupportedSystem (
-        { pkgs }:
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              pkg-config
-              libxkbcommon
-              wayland
+          LD_LIBRARY_PATH = "${pkgs.libGL}/lib";
+          cargoBuildType = "debug";
+          cargoCheckType = "debug";
 
-              cargo
-              rustc
-              rust-analyzer
-              rustfmt
-              labwc
-            ];
-          };
-        }
-      );
-    };
+          dontStrip = true;
+
+          postInstall = ''
+            wrapProgram $out/bin/cthulock --prefix LD_LIBRARY_PATH : "${
+                pkgs.lib.makeLibraryPath [
+                  pkgs.wayland
+                  pkgs.libGL
+                  pkgs.fontconfig
+                ]
+              }"
+          '';
+        };
+        devShells.default = pkgs.mkShell {
+          inherit buildInputs nativeBuildInputs;
+          packages = with pkgs; [
+            cargo
+            rustc
+            rust-analyzer
+            rustfmt
+            labwc
+          ];
+          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
+        };
+      }
+    );
 }
